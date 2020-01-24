e7a9001ee7916c293ecb54b06cd0daf431fa88db
Dan Cordner <takamine16@gmail.com>
2020-01-24 11:48:57 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lasttry
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
